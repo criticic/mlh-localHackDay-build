75467a6e02c8ff0a1f09e576dd25d6b213b2e228
Sagnik Mandal <sonalisagnik123@gmail.com>
2021-01-11 01:02:21 +0530

    completed Challenge: Hello World In Another Programming Language

---
+++ day-one/hello-world/submission.v
@@ -0,0 +1,7 @@
+// This file can be run by downloading Vlang from https://github.com/vlang/v
+
+// Run `v run submission.v` to run this file.
+
+fn main() {
+	println('lets get hacking #MLH #LocalHackDay #Eddiehub')
+}
